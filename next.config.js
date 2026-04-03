b4eb951b56ab6cf16cb58ff722714c8693a53b79
lucaott-jpg <luca.ott@aurumasset.us>
2026-04-03 12:39:31 -0400

    fix pdfjs canvas webpack

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['wqkatdcaqygnmnbealqg.supabase.co'],
+  webpack: (config) => {
+    config.externals = [...(config.externals || []), { canvas: 'canvas' }]
+    return config
   },
 }
-
 module.exports = nextConfig
